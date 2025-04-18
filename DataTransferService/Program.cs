80c29f9565ec44f560826dbe075e50699c9e021f
송선민 <102493442+ipodsong2i@users.noreply.github.com>
2025-04-18 11:28:12 +0900

    Update: AppSetting 추가 및 테스트 프로젝트 생성
    AppSetting을 싱글톤으로 등록하여 사용가능하도록 설정
    XUnit 테스트 프로젝트 생성

--- DataTransferService/Program.cs
+++ DataTransferService/Program.cs
@@ -1,11 +1,16 @@
+using DataTransferService.Configs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Register a singleton instance of AppSettings using the configuration from the builder
+builder.Services.AddSingleton(new AppSettings(builder.Configuration));
 
 var app = builder.Build();
 
