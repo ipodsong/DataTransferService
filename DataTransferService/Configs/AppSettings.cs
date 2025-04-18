efb6c851061dc7977cda596b65b3c884496af01e
송선민 <102493442+ipodsong2i@users.noreply.github.com>
2025-04-18 11:28:12 +0900

    Update: Added AppSetting and Created Test Project
    Configured AppSetting to be registered as a singleton for application-wide usage.
    Created an XUnit test project.

---
+++ DataTransferService/Configs/AppSettings.cs
@@ -0,0 +1,18 @@
+﻿namespace DataTransferService.Configs
+{
+    public class AppSettings
+    {
+        #region Fields
+        private static string? _applicationMode;
+        #endregion
+
+        #region Properties
+        public static string? ApplicationMode { get => _applicationMode; }
+        #endregion
+
+        public AppSettings(IConfigurationRoot config)
+        {
+            _applicationMode = config.GetSection("ApplicationMode").Get<string>();
+        }
+    }
+}
